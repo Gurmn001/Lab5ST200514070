9e8d8f0f5c56059370374373122699858ac0e0f0
Gurmn001 <200514070@student.georgianc.on.ca>
2023-02-16 17:06:45 -0500

    Complete

--- src/main/java/com/example/lab5st200514070/MobilePhone.java
+++ src/main/java/com/example/lab5st200514070/MobilePhone.java
@@ -1,12 +1,13 @@
-package com.example.mobilephone;
+package com.example.lab5st200514070;
+import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 
 public class MobilePhone {
 
     private String name;
-    private Image image;
+    private String image;
 
-    public MobilePhone(String name, Image image) {
+    public MobilePhone(String name, String image) {
         this.name = name;
         this.image = image;
     }
@@ -15,7 +16,8 @@ public class MobilePhone {
         return name;
     }
 
-    public Image getImage() {
+    public String getImage() {
         return image;
     }
+
 }
